f7394fd54bf66e7afba415afeff3921de12a0f49
rnjs5540 <rnjs5540@gmail.com>
2023-11-24 23:25:34 +0900

    요걸로

--- src/main/java/com/mysite/sbb/controller/CodyController.java
+++ src/main/java/com/mysite/sbb/controller/CodyController.java
@@ -16,22 +16,24 @@ import org.openqa.selenium.chrome.ChromeOptions;
 //@CrossOrigin(allowedHeaders = "*")
 public class CodyController {
     @PostMapping("/codies")
-    public List<CodyResponseBody> crawling_codies(@RequestBody CodyRequestBody requestBody){
+    public List<CodyResponseBody> crawling_codies(@RequestBody CodyRequestBody requestBody)
+            throws InterruptedException {
 
         final int CODY_COUNT = 12;
         System.setProperty("webdriver.chrome.driver", "./chromedriver.exe"); // 윈도우
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+//        options.addArguments("--remote-allow-origins=*");
         WebDriver driver = new ChromeDriver(options);
 
         // 무신사 코디맵 페이지 접속
-        driver.get("https://www.musinsa.com/app/codimap/lists?tag_no=" + requestBody);
-
-        // 1. 드라이버 설치 경로
-        public static String WEB_DRIVER_ID = "webdriver.chrome.driver";
-        public static String WEB_DRIVER_PATH = "C:/chromedriver/chromedriver.exe";
+//        driver.get("https://www.musinsa.com/app/codimap/lists?tag_no=" + requestBody.getTag());
+        driver.get("https://www.naver.com");
 
         List<CodyResponseBody> responseBodies = new ArrayList<>();
+
+            wait(1000);
+
+
 
         // 셀레니움으로 responseBodies에 코디들 넣기
 
