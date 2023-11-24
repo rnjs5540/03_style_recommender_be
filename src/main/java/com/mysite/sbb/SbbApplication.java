1848acd500e2c5d9282b15643412d6c092a7dd1a
rnjs5540 <rnjs5540@gmail.com>
2023-11-24 23:55:39 +0900

    afsdfs

--- src/main/java/com/mysite/sbb/SbbApplication.java
+++ src/main/java/com/mysite/sbb/SbbApplication.java
@@ -1,5 +1,9 @@
 package com.mysite.sbb;
 
+import com.mysite.sbb.controller.CodyController;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +11,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SbbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbbApplication.class, args);
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe"); // 윈도우
+
+		ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+//		options.addArguments("--no-sandbox");
+//		options.addArguments("--disable-dev-shm-usage");
+		WebDriver driver = new ChromeDriver(options);
+
+		// 무신사 코디맵 페이지 접속
+//        driver.get("https://www.musinsa.com/app/codimap/lists?tag_no=" + requestBody.getTag());
+//		driver.get("https://www.naver.com");
+
+
+
+
+//		SpringApplication.run(SbbApplication.class, args);
 	}
 
 }
